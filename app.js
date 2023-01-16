00966fc5a07a945edcd2e11d5ec8353289c6a382
Hafid Qarchi <haqarchi@gmail.com>
2023-01-16 21:54:04 +0900

    get all task initial

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import express from "express";
 const app = express();
+import tasksRouter from "./routes/tasks.js";
 const PORT = 3000;
 
-app.listen(PORT, console.log(""));
+app.get("/hello", (req, res) => {
+  res.send("Task manager App");
+});
+
+app.use("api/v1/tasks", tasksRouter);
+
+app.listen(PORT, console.log(`Listening on port ${PORT}...`));
