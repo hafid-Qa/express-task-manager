889da17b145799272cd841ebf93d298805607cfa
Hafid Qarchi <haqarchi@gmail.com>
2023-01-16 22:48:33 +0900

    all routes return json data

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,16 +2,16 @@ const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
 const createTask = (req, res) => {
-  res.send("create a task");
+  res.json(req.body);
 };
 const getTask = (req, res) => {
-  res.send("get single task");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
-  res.send("update a task");
+  res.json({ id: req.params.id });
 };
 const deleteTask = (req, res) => {
-  res.send("delete a task");
+  res.json({ id: req.params.id });
 };
 
 export { getAllTasks, createTask, getTask, updateTask, deleteTask };
