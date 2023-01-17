918f8261e1c6bfc64307b70635c7a953e97b17ca
Hafid Qarchi <haqarchi@gmail.com>
2023-01-17 22:49:52 +0900

    intial server error handling middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from "./db/connect.js";
 import tasksRouter from "./routes/tasks.js";
 import notFound from "./middleware/not-found.js";
+import errorHandler from "./middleware/error-handler.js";
 const PORT = 3000;
 
 const connectionString = process.env.MONGO_DB_CONNECTION;
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasksRouter);
 // error handlers for wrong routes
 app.use(notFound);
+// error handler for server errors
+app.use(errorHandler);
 const start = async () => {
   try {
     await connectDB(connectionString);
