6b773e4c3c24376c475f777caacc26ad046e5510
Hafid Qarchi <haqarchi@gmail.com>
2023-01-17 20:11:00 +0900

    task model validation

--- models/task.js
+++ models/task.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, "Task name is required"],
+    trim: true,
+    maxlength: [50, "Name cant not exceed 50 characters"],
+  },
   completed: {
     type: Boolean,
     default: false,
