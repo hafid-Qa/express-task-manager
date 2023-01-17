2a03b743b0d13da7fbf21d1dce48a61fe89c4bcb
Hafid Qarchi <haqarchi@gmail.com>
2023-01-17 16:30:37 +0900

    import db connection string in app from .env file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-import connectDB from "./db/connect.js";
 import express from "express";
 const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/connect.js";
 import tasksRouter from "./routes/tasks.js";
 const PORT = 3000;
 
+const connectionString = process.env.MONGO_DB_CONNECTION;
+console.log(connectionString);
 // middleware
 app.use(express.json());
 
@@ -16,7 +20,7 @@ app.use("/api/v1/tasks", tasksRouter);
 
 const start = async () => {
   try {
-    connectDB();
+    await connectDB(connectionString);
     app.listen(PORT, console.log(`Listening on port ${PORT}...`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
-import * as dotenv from "dotenv";
-dotenv.config();
 
-const connectionString = process.env.MONGO_DB_CONNECTION;
-console.log(connectionString);
 
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
