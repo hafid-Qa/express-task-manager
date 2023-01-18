1a9ac84bfab2071b752262cb5ce84c10383d9b73
Hafid Qarchi <haqarchi@gmail.com>
2023-01-18 15:03:43 +0900

    typo in status code var

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
+import { CustomAPIError } from "../errors/custom-error.js";
+
 const errorHandler = (err, req, res, next) => {
-  return res.status(err.status).json({ msg: err.message });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  return res.status(500).json({ msg: "Something went wrong,Please try again" });
 };
 
 export default errorHandler;
